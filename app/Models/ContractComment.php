841ba51551ae70d66f65be9ff5a00f9b55ca1728
ked-k <kedkayz@gmail.com>
2022-12-05 22:16:04 +0300

    initial

---
+++ app/Models/ContractComment.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ContractComment extends Model
+{
+    protected $fillable = [
+        'contract_id',
+        'comment',
+        'created_by',
+    ];
+    
+    public function client()
+    {
+        return $this->hasOne('App\Models\Customer', 'id', 'created_by');
+    }
+}
